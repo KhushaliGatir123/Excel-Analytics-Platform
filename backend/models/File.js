c2d955ed4f36b791853833856db85af9e77f58ca
Khushali Gatir <gatirkhushali456@gmail.com>
2025-07-25 02:03:30 +0530

    version 3 best version

--- backend/models/File.js
+++ backend/models/File.js
@@ -1,24 +1,9 @@
 const mongoose = require("mongoose");
-
-const fileSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  filename: {
-    type: String,
-    required: true,
-  },
-  data: [
-    {
-      type: Object,
-    },
-  ],
-  uploadDate: {
-    type: Date,
-    default: Date.now,
-  },
+const FileSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  filename: { type: String, required: true },
+  path: { type: String, required: true },
+  data: { type: Array, required: true },
+  uploadDate: { type: Date, default: Date.now },
 });
-
-module.exports = mongoose.model("File", fileSchema);
+module.exports = mongoose.model("File", FileSchema);
--- backend/models/Graph.js
+++ backend/models/Graph.js
@@ -1,31 +1,10 @@
 const mongoose = require("mongoose");
-
-const graphSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  fileName: {
-    type: String,
-    required: true,
-  },
-  xAxis: {
-    type: String,
-    required: true,
-  },
-  yAxis: {
-    type: String,
-    required: true,
-  },
-  types: {
-    type: [String],
-    required: true,
-  },
-  timestamp: {
-    type: String,
-    required: true,
-  },
+const GraphSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  fileName: { type: String, required: true },
+  xAxis: { type: String, required: true },
+  yAxis: { type: String, required: true },
+  types: { type: [String], required: true },
+  timestamp: { type: String, required: true },
 });
-
-module.exports = mongoose.model("Graph", graphSchema);
+module.exports = mongoose.model("Graph", GraphSchema);
